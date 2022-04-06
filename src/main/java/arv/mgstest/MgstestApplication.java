41880efc4d3c4d7161bf5bd905847e604488583f
ArvikVan <arvikvan@gmail.com>
2022-04-06 09:47:15 +0300

    init_project

---
+++ src/main/java/arv/mgstest/MgstestApplication.java
@@ -0,0 +1,13 @@
+package arv.mgstest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MgstestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MgstestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/arv/mgstest/MgstestApplicationTests.java
@@ -0,0 +1,13 @@
+package arv.mgstest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MgstestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
